0264e181c4938758ebca28bb10034323e96340aa
Damla Kara <kdamla41@gmail.com>
2023-03-03 22:03:33 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import AddPostForm from "./features/posts/AddPostForm";
+import PostsLits from "./features/posts/PostsLits";
+function App() {
+  return (
+    <main className="App">
+        <AddPostForm />
+        <PostsLits/>
+
+    </main>
+  );
+}
+
+export default App;
